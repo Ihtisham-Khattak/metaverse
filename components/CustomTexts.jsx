5cb5b573e8ccfaf4c6065039055ac2a3b7602f3a
Ihtisham-Khattak <ikpashteen1@gmail.com>
2023-04-29 12:38:09 +0500

    about  section

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,22 @@
-'use client';
+"use client";
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { motion } from "framer-motion";
+import { textVariant2, textContainer } from "../utils/motion";
+
+export const TypingText = ({ title, textAlign }) => (
+  <motion.p
+    variants={textContainer}
+    initial="hidden"
+    whileInView="show"
+    className={`${textAlign} font-normal text-[20px] text-secondary-white`}
+  >
+    {/* The from() method creates a new array from any array-like or iterable object.*/}
+    {Array.from(title).map((letter, index) => (
+      <motion.span variants={textVariant2} key={index}>
+        {letter === " " ? "\u00A0" : letter}
+      </motion.span>
+    ))}
+  </motion.p>
 );
 
-export const TitleText = () => (
-  <h2>Title Text</h2>
-);
+export const TitleText = ({ title, textAlign }) => <h2>Title Text</h2>;
