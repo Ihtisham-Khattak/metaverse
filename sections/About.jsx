8a7bef9690acc6dbfe06d5d4312b88e7ee554453
Ihtisham-Khattak <ikpashteen1@gmail.com>
2023-05-01 14:27:33 +0500

    complete about section

--- sections/About.jsx
+++ sections/About.jsx
@@ -17,8 +17,36 @@ const About = () => (
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
       <TypingText title="| About Metaverse" textAlign="text-center" />
+
+      <motion.p
+        variants={fadeIn("up", "tween", 0.2, 1)}
+        initial="hidden"
+        whileInView="show"
+        className="text-secondary-white mt-[20px] sm:text-[24px] text-[20px] font-normal text-center "
+      >
+        <span className="font-extrabold uppercase"> Metaverse</span> is a new
+        thing in the future, where you can enjoy the virtual world by feeling
+        like it's really real, you can feel what you feel in this metaverse
+        world, because this is really the
+        <span className="font-extrabold uppercase">
+          madness of the metaverse
+        </span>{" "}
+        of today, using only <span className="font-extrabold">VR</span> 
+        devices you can easily explore the metaverse world you want, turn your
+        dreams into reality. Let's{" "}
+        <span className="font-extrabold uppercase">explore</span> the madness of
+        the metaverse by scrolling down
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        initial="hidden"
+        whileInView="show"
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[18px] mt-[10px] object-contain"
+      />
     </motion.div>
-    About me
   </section>
 );
 
